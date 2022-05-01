562bc273568efd457d012d12c3cfe2cde3cf79db
scott williams <charlesscottwilliams@gmail.com>
2022-05-01 17:57:39 -0600

    updated uri

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
-  cache: new InMemoryCache(),
-});
+	uri: 'https://catstronauts-server227.herokuapp.com/',
+	cache: new InMemoryCache(),
+})
 
 ReactDOM.render(
   <ApolloProvider client={client}>
